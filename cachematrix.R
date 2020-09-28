c5a2ea8380ca5254f501b33cf4ad3ee77ebdb7a4
Hadia Akbar <h.akbar@aggiemail.usu.edu>
2020-09-28 12:14:51 +0500

    Add second function Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,41 @@
+#Programming Assignment 2
+#Caching Inverse of a matrix
+
+
+#The first part of the create a function to create matrix, 
+#retrive the matrix, create inverse of matrix and get the inverse.
 
 makeCacheMatrix <- function(x = matrix()){
   inv = NULL
-  setmatrix <- function(y){
+
+    setmatrix <- function(y){
     x <<- y
     inv <<- NULL
   }
   getmatrix <- function()(x)
+  
   setInverse <- function(inverse)(inv <<- inverse)
-  getInverse <- function(inv)
+  getInverse <- function()(inv)
+  
   list(setmatrix=setmatrix, getmatrix=getmatrix, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This part of code solves the matrizx if a matrix is given as input
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)){
+    message("acquiring cached data")
+    return(inv)
+  }
+  m <- x$getmatrix()
+  inv <- solve(m, ..)
+  x$setInverse(inv)
+  inv
 }
+
+##Example : Create a 4x4 matrix using
+##          mat <- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2))
+##command the function to create and cache the inverse of matrix using
+#           cachesolve(mat)
