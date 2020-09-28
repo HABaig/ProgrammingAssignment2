5384a6da46aae41821055a65ac90842d08267333
Hadia Akbar <h.akbar@aggiemail.usu.edu>
2020-09-28 11:43:36 +0500

    Added first function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  inv = NULL
+  setmatrix <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  getmatrix <- function()(x)
+  setInverse <- function(inverse)(inv <<- inverse)
+  getInverse <- function(inv)
+  list(setmatrix=setmatrix, getmatrix=getmatrix, setInverse=setInverse, getInverse=getInverse)
 }
 
 
